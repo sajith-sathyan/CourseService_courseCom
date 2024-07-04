861fc7780b0603d96b8733e188a3ffeafd832c49
sajith-sathyan <113768183+sajith-sathyan@users.noreply.github.com>
2024-07-04 08:07:52 +0530

    Update server.js

--- frameworks/webserver/server.js
+++ frameworks/webserver/server.js
@@ -1,19 +1,36 @@
 export default function serverConfig(app, mongoose, config) {
-  // Connect to MongoDB
-  console.log("config.uri ----->", config.uri);
-  mongoose
-    .connect(
-      "mongodb+srv://sajith:i8hsM4JR7SzkB2YV@cluster0.c9y6zm1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    )
-    .then(() => console.log("Connected to MongoDB"))
-    .catch((err) => console.error("Error connecting to MongoDB:", err));
+  const mongoUri = process.env.MONGODB_URI || 'mongodb+srv://sajithsathyandeveloper:Ad9qE5cg2ziTeYBm@cluster0.zcisrml.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+
+  mongoose.connect(mongoUri);
+
+  const db = mongoose.connection;
+
+  db.on('connected', () => {
+    console.log('Connected to MongoDB');
+  });
+
+  db.on('error', (err) => {
+    console.error(`MongoDB connection error: ${err.message}`);
+  });
+
+  db.on('disconnected', () => {
+    console.log('Disconnected from MongoDB');
+  });
+
+  process.on('SIGINT', () => {
+    db.close(() => {
+      console.log('MongoDB connection closed through app termination');
+      process.exit(0);
+    });
+  });
 
   function startServer() {
     app.listen(config.port, () => {
-      console.log(`UserAuth Server Start At port${config.port}`);
+      console.log(`UserAuth Server Start At port ${config.port}`);
     });
   }
+
   return {
-    startServer,
+    startServer
   };
 }
